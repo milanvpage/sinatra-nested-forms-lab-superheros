b621738e0d3a8d6875014fee9ebf141318ee7fce
Milan <milanvpage@gmail.com>
2021-05-26 21:55:35 -0700

    commit tree bc3b5875d4660944d0063db5465c1bfcf480ff95

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,15 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+    
+    get "/" do
+        erb :team
+    end
 
+    post '/teams' do
+        @team = params
+
+        erb :super_hero        
+    end
 
 end
